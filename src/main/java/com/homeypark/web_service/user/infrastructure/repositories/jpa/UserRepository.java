700b923ebc5642bdd50f15081103136585462d5c
SgtGarro <marcelogarro137@gmail.com>
2024-09-12 08:21:11 -0500

    feat (US-01): create user entity

---
+++ src/main/java/com/homeypark/web_service/user/infrastructure/repositories/jpa/UserRepository.java
@@ -0,0 +1,4 @@
+package com.homeypark.web_service.user.infrastructure.repositories.jpa;
+
+public class UserRepository {
+}
---
+++ src/main/java/com/homeypark/web_service/user/interfaces/rest/UserControllers.java
@@ -0,0 +1,4 @@
+package com.homeypark.web_service.user.interfaces.rest;
+
+public class UserControllers {
+}
