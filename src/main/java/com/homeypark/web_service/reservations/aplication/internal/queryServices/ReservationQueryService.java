22985282601a605829959957896f7d81adb4d8aa
VittorioEdu <u20231b547@upc.edu.pe>
2024-09-12 13:12:59 -0500

    feat (US-09): implement query reservation service in controller

--- src/main/java/com/homeypark/web_service/reservations/aplication/internal/queryServices/ReservationQueryService.java
+++ src/main/java/com/homeypark/web_service/reservations/aplication/internal/queryServices/ReservationQueryService.java
@@ -4,9 +4,11 @@ import com.homeypark.web_service.reservations.domain.model.entities.Reservation;
 import com.homeypark.web_service.reservations.domain.model.queries.GetReservationByIdQuery;
 import com.homeypark.web_service.reservations.domain.services.IReservationQueryService;
 import com.homeypark.web_service.reservations.infrastructure.repositories.jpa.IReservationRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class ReservationQueryService implements IReservationQueryService {
 
     private final IReservationRepository reservationRepository;
--- src/main/java/com/homeypark/web_service/reservations/interfaces/rest/ReservationController.java
+++ src/main/java/com/homeypark/web_service/reservations/interfaces/rest/ReservationController.java
@@ -1,9 +1,12 @@
 package com.homeypark.web_service.reservations.interfaces.rest;
 
 import com.homeypark.web_service.reservations.aplication.internal.commandServices.ReservationCommandService;
+import com.homeypark.web_service.reservations.aplication.internal.queryServices.ReservationQueryService;
 import com.homeypark.web_service.reservations.domain.model.entities.Reservation;
+import com.homeypark.web_service.reservations.domain.model.queries.GetReservationByIdQuery;
 import com.homeypark.web_service.reservations.interfaces.rest.resources.CreateReservationResource;
 import com.homeypark.web_service.reservations.interfaces.rest.transformers.CreateReservationCommandFromResourceAssembler;
+import com.homeypark.web_service.user.domain.model.queries.GetUserByIdQuery;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,10 +15,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/reservations")
 public class ReservationController {
     private final ReservationCommandService reservationCommandService;
+    private final ReservationQueryService reservationQueryService;
 
 
-    public ReservationController(ReservationCommandService reservationCommandService) {
+    public ReservationController(ReservationCommandService reservationCommandService, ReservationQueryService reservationQueryService) {
         this.reservationCommandService = reservationCommandService;
+        this.reservationQueryService = reservationQueryService;
     }
 
     @PostMapping
@@ -25,5 +30,14 @@ public class ReservationController {
         var reservation = reservationCommandService.handle(createReservationCommand);
 
         return reservation.map(r -> new ResponseEntity<>(r, HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.BAD_REQUEST));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Reservation> getReservationById(@PathVariable("id") Long id) {
+        var getReservationByIdQuery = new GetReservationByIdQuery(id);
+
+        var reservation = reservationQueryService.handle(getReservationByIdQuery);
+
+        return reservation.map(r -> new ResponseEntity<>(r, HttpStatus.OK)).orElse(ResponseEntity.notFound().build());
     }
 }
